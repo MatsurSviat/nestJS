1f88038a5fcf978fd8ab30385f19220026c65f3d
MatsurSviat <hereyourare@rambler.ru>
2023-03-01 22:29:28 +0300

    feat: delete node_module and dist folders

--- trainee/src/app.module.ts
+++ trainee/src/app.module.ts
@@ -1,17 +1,15 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
 
 @Module({
   imports: [
     AuthModule,
     UserModule,
-    MongooseModule.forRoot(
-      `mongodb+srv://sviat:sviat123@cluster0.99ienvd.mongodb.net/?retryWrites=true&w=majority`,
-    ),
+    MongooseModule.forRoot(`mongodb://localhost/nest`),
   ],
   controllers: [AppController],
   providers: [AppService],
